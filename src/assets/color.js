da00e1f554ea40baee84786b5dc2b170c15a1205
Jerika Kanz <jerikacapila@gmail.com>
2022-10-01 20:02:00 +0200

    Add Colors Data

---
+++ src/assets/color.js
@@ -0,0 +1,9 @@
+import { nanoid } from "nanoid";
+
+export const colorsData = [
+  { id: nanoid(), hexColor: "#B9E28C" },
+  { id: nanoid(), hexColor: "#5B618A" },
+  { id: nanoid(), hexColor: "#9B5094" },
+  { id: nanoid(), hexColor: "#EF5B5B" },
+  { id: nanoid(), hexColor: "#20A39E" },
+];
