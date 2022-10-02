e8ad048977507828bdf523dc4868422bf3085ea0
Jerika Kanz <jerikacapila@gmail.com>
2022-10-02 19:56:38 +0200

    Add localstorage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { nanoid } from "nanoid";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { colorsData } from "./assets/color";
 
 import Form from "./components/form/Form";
 import ColorCard from "./components/colorCard/ColorCard";
+import { saveToLocal, loadFromLocal } from "./lib/localStorage";
 
 import "./App.css";
 
 function App() {
-  const [colorList, setColorList] = useState(colorsData);
+  const [colorList, setColorList] = useState(
+    loadFromLocal("saved colors") ?? colorsData
+  );
 
   const addColorCard = (color) => {
     setColorList([
@@ -30,6 +33,10 @@ function App() {
       deleteColor={deleteColorCard}
     />
   ));
+
+  useEffect(() => {
+    saveToLocal("saved colors", colorList);
+  }, [colorList]);
 
   return (
     <>
