b84220d627f69102ebb9aee4238f5eab005f335d
Jerika Kanz <jerikacapila@gmail.com>
2022-10-02 18:58:24 +0200

    Add color name

--- src/components/colorCard/ColorCard.js
+++ src/components/colorCard/ColorCard.js
@@ -1,15 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ColorCard({ id, colorCode, deleteColor }) {
   const [newColor, setNewColor] = useState(colorCode);
+  const [colorName, setNewColorName] = useState("");
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        `https://www.thecolorapi.com/id?hex=${newColor.slice(1)}`
+      );
+      const data = await response.json();
+      setNewColorName(data.name.value);
+    }
+    getData();
+  }, [newColor]);
 
   return (
     <li className="card" style={{ backgroundColor: `${newColor}` }}>
       <button className="card__btn" onClick={() => deleteColor(id)}>
         ×
       </button>
+      <p className="card__name">{colorName}</p>
       <input
-        className="card__text"
+        className="card__input"
         type="text"
         defaultValue={newColor}
         onClick={(e) => navigator.clipboard.writeText(e.target.value)}
