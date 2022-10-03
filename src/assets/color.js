12f3d8d36f4ce471769183fc339ec0618765aefd
Jerika Kanz <jerikacapila@gmail.com>
2022-10-03 20:03:55 +0200

    Update Color Data

--- src/assets/color.js
+++ src/assets/color.js
@@ -1,9 +1,26 @@
 import { nanoid } from "nanoid";
 
 export const colorsData = [
-  { id: nanoid(), hexColor: "#B9E28C" },
-  { id: nanoid(), hexColor: "#5B618A" },
-  { id: nanoid(), hexColor: "#9B5094" },
-  { id: nanoid(), hexColor: "#EF5B5B" },
-  { id: nanoid(), hexColor: "#20A39E" },
+  {
+    id: nanoid(),
+    title: "Color Palette 1",
+    colors: [
+      { id: nanoid(), hexCode: "#B9E28C" },
+      { id: nanoid(), hexCode: "#5B618A" },
+      { id: nanoid(), hexCode: "#9B5094" },
+      { id: nanoid(), hexCode: "#EF5B5B" },
+      { id: nanoid(), hexCode: "#20A39E" },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: "Color Palette 2",
+    colors: [
+      { id: nanoid(), hexCode: "#CFFFE5" },
+      { id: nanoid(), hexCode: "#FFAD69" },
+      { id: nanoid(), hexCode: "#F5E663" },
+      { id: nanoid(), hexCode: "#6F2DBD" },
+      { id: nanoid(), hexCode: "#E13700" },
+    ],
+  },
 ];
